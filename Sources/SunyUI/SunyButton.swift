b31a3424ccca79b48ef46d4c70360b4e40edeb7e
Sana-Ullah <sana-ullah@192.168.1.5>
2025-11-14 15:28:27 +0500

    Initial commit

---
+++ Sources/SunyUI/SunyButton.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  SunyUI
+//
+//  Created by Sana-Ullah on 14/11/2025.
+//
+
+import Foundation
+import SwiftUI
+
+public struct SunyButton: View {
+    
+    var action: () -> ()
+    
+    public var body: some View {
+        Button {
+            action()
+        } label: {
+            Text("SunyButtonTap")
+        }
+    }
+}
